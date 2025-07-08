b218a3ee2d103457a1ca6cd480f04296330dcaaa
KashishRathod17 <rathodkashish03@gmail.com>
2025-07-08 22:55:00 +0530

    Secure config by using environment variables

--- config.py
+++ config.py
@@ -1,8 +1,13 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 class Config:       
-    DB_USER = 'root'
-    DB_PASSWORD = 'kashish1234'
-    DB_SERVER = 'localhost'
-    DB_DATABASE = 'flaskapp'
+    DB_USER = os.getenv("DB_USER")
+    DB_PASSWORD = os.getenv("DB_PASSWORD")
+    DB_SERVER = os.getenv("DB_SERVER","localhost")
+    DB_DATABASE = os.getenv("DB_DATABASE")
 
     SQLALCHEMY_DATABASE_URI = f'mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_SERVER}/{DB_DATABASE}'
     SQLALCHEMY_TRACK_MODIFICATIONS =  True
